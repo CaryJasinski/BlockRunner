abd412f47f4147c65dcec85255d6e80b570b4421
Cary Jasinski <cary.jasinski@loop.colum.edu>
2014-03-05 23:40:10 -0600

    Added 2D Project with Textures

---
+++ NeonKnight/Assets/Scripts/ClickAndRotate.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickAndRotate : MonoBehaviour {
+	[HideInInspector] public Vector3 StartPosition = new Vector3(0,0,270);
+	[HideInInspector] public Vector3 EndPosition = new Vector3(0,0,90);
+	public bool StartPos = true;
+
+	void OnMouseDown() {
+		if (StartPos) {
+			transform.Rotate(EndPosition);
+			StartPos = false;
+		} else {
+			transform.Rotate(StartPosition);
+			StartPos = true;
+		}
+	}
+}
---
+++ NeonKnight/Assets/Scripts/PlatMovement.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatMovement : MonoBehaviour {
+	private Vector3 offset;
+	public Vector2 FirstPosition;
+	public Vector2 SecondPosition;
+	public GameObject positionDot;
+	public int secondPositionOffset = 6;
+	public bool Vertical = false;
+	public bool Moveable = false;
+	
+	void Start () 
+	{
+		if(Moveable)
+		{
+			FirstPosition = transform.position;
+			Debug.Log(FirstPosition);
+			if(Vertical)
+			{
+				SecondPosition = new Vector2(FirstPosition.x, FirstPosition.y + secondPositionOffset);
+			}
+			else
+			{
+				SecondPosition = new Vector2(FirstPosition.x + secondPositionOffset, FirstPosition.y);
+			}
+			Instantiate(positionDot, FirstPosition, Quaternion.identity);
+			Instantiate(positionDot, SecondPosition, Quaternion.identity);
+		}
+	}
+	void Update () 
+	{
+		foreach (Touch touch in Input.touches) 
+		{
+			if (touch.phase == TouchPhase.Began)
+			{
+				Ray ray = Camera.main.ScreenPointToRay (touch.position);
+				if (Physics.Raycast (ray)) {
+					Instantiate (positionDot, transform.position, transform.rotation);
+				}
+			}
+		}
+	}
+	void OnMouseDown() 
+	{
+		if(Moveable)
+		{
+			offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+		}
+	}
+	
+	void OnMouseDrag () 
+	{
+		if(Moveable)
+		{
+			if(Vertical) 
+			{
+
+			} 
+			else 
+			{
+
+			}
+		}
+	}
+}
