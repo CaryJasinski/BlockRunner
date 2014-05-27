6ce8c327c43fa597f4c4814cf0a3b0279d9decfc
Christina <christina.dattomo@loop.colum.edu>
2014-05-27 12:45:18 -0500

    Pause Game / Return to Main Menu Implemented
    A basic GUI for a pause button that leads to a menu with "Resume" and
    "Main Menu".
    Pauses Neon Knight, but not ability to move platforms (Will fix!)

---
+++ NeonKnight/Assets/Scripts/Gameplay/Pausing.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pausing : MonoBehaviour {
+
+	public bool trigPause = false;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Update()
+	{
+		if(trigPause)
+			Pause();
+		    PauseMenu();
+		if(!trigPause)
+		 Time.timeScale = 1.0F;
+	}
+
+	void OnGUI()
+	    {
+		  if (GUI.Button (new Rect (0,Screen.height - 50,100,50), "Pause"))
+			{
+				Debug.Log("you clicked");
+				trigPause = true;
+	   	    }
+		 
+		if(trigPause)
+			PauseMenu();
+	}
+
+	void Pause()
+	{
+		if(trigPause)
+			Time.timeScale = 0F;
+	}
+
+	void PauseMenu()
+	{
+		if (GUI.Button (new Rect (Screen.width/2,Screen.height/2,100,50), "Main Menu"))
+			Application.LoadLevel("MainMenu");
+		if (GUI.Button (new Rect (Screen.width/2,Screen.height/2 - 50,100,50), "Resume"))
+			trigPause = false;
+	}
+
+}
