6b37be28c3327aa369a9b958435a67a0a957b83a
Cary Jasinski <cary.jasinski@loop.colum.edu>
2015-01-07 00:51:05 -0600

    Setup for MajorRevisions Branch

--- NeonKnight/Assets/Scripts/UI/UIManager/InGameUIManager.cs
+++ NeonKnight/Assets/Scripts/UI/UIManager/InGameUIManager.cs
@@ -8,7 +8,14 @@ public class InGameUIManager : MonoBehaviour
 	public Canvas inGameUICanvas;
 	public Text lifeTextHolder;
 	public Text collectibleTextHolder;
-	
+	public Text maxCollectibleTextHolder;
+
+	//Use this for initialization
+	public void EnableOverlay(bool enabled)
+	{
+		inGameUICanvas.enabled = enabled;
+	}
+
 	void Update ()
 	{
 		if(inGameUICanvas.enabled)
@@ -16,10 +23,5 @@ public class InGameUIManager : MonoBehaviour
 			lifeTextHolder.text = PersistantData.persistantDataController.playerLives.ToString();
 			collectibleTextHolder.text = PersistantData.persistantDataController.bits.ToString();
 		}
-	}
-
-	public void EnableOverlay(bool enabled)
-	{
-		inGameUICanvas.enabled = enabled;
 	}
 }
